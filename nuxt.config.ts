44b6c788ea5a2f81bf32d4696299455006a9570e
Achmat Fauzi <achmatfauzi86@gmail.com>
2024-12-21 14:35:32 +0700

    update template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,82 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // https://nuxt.com/modules
-  modules: [
-    '@nuxthub/core',
-    '@nuxt/eslint',
-  ],
-
-  // https://devtools.nuxt.com
-  devtools: { enabled: true },
-
-  // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
+  srcDir: './src',
   runtimeConfig: {
     public: {
-      // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: 'Hello from the Edge 👋',
+      helloText: 'Hello',
     },
   },
-  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2024-07-30',
 
-  // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
     database: true,
     kv: true,
   },
 
-  // https://eslint.nuxt.com
+  modules: [
+    '@nuxthub/core',
+    '@nuxt/eslint',
+    '@nuxtjs/google-fonts',
+    'dayjs-nuxt',
+    '@nuxtjs/device',
+    '@morev/vue-transitions/nuxt',
+    '@hypernym/nuxt-gsap',
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: ['defineStore', 'acceptHMRUpdate', 'useDayjs'],
+        storesDirs: ['./src/stores/**'],
+      },
+    ],
+  ],
+  
+  googleFonts: {
+    families: {
+      Inter: '300..800',
+    },
+    display: 'swap',
+    preload: true,
+    useStylesheet: true,
+    base64: true,
+    download: true,
+  },
+  tailwindcss: {
+    cssPath: ['~/assets/styles/index.scss', { injectPosition: 'first' }],
+    configPath: 'tailwind.config',
+    exposeConfig: {
+      level: 2,
+    },
+    config: {},
+    // viewer: true,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: [
+    '~/assets/styles/index.scss',
+  ],
+
+  devtools: { enabled: true },  
+
   eslint: {
     config: {
       stylistic: {
         quotes: 'single',
       },
     },
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler' // or "modern"
+        }
+      }
+    }
   },
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fixed top-0 left-0 w-full h-full flex justify-center items-center">
+    <div class="text-5xl">
+      Hello
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'main'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
